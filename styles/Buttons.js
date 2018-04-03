b7ca90aa50c4edaec11f4331c34e43de2cb90f57
mhutchison90 <michaelhutchison90@gmail.com>
2018-04-03 13:19:22 -0600

    added a styles folder for styled components

---
+++ styles/Buttons.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ButtonPush = styled.TouchableOpacity`
+  flex: 1;
+  padding-top: 500px;
+  padding-horizontal: 15px;
+`;
---
+++ styles/Inputs.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const AccountInput = styled.TextInput`
+  flex: 1;
+  padding-top: 50px;
+  padding-horizontal: 15px;
+`;
